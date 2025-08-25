e95270c1be69937bc5cf65809ddab53af4b359e2
Dominik Kluske <dome.k28@yahoo.com>
2025-08-25 19:14:12 +0200

    implement theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,21 @@ import Diary from "@/pages/Diary.tsx";
 import Settings from "@/pages/Settings.tsx";
 import Navbar from "@/components/ui/Navbar.tsx";
 import NotFound from "@/pages/NotFound.tsx";
+import {ThemeProvider} from "@/lib/ThemeProvider.tsx";
 
 function App() {
   return (
       <BrowserRouter>
-          <h1>OpenCalories</h1>
-          <Navbar/>
-          <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/diary" element={<Diary/>}/>
-              <Route path="/settings" element={<Settings/>}/>
-              <Route path="*" element={<NotFound/>}/>
-          </Routes>
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+              <h1>OpenCalories</h1>
+              <Navbar/>
+              <Routes>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="/diary" element={<Diary/>}/>
+                  <Route path="/settings" element={<Settings/>}/>
+                  <Route path="*" element={<NotFound/>}/>
+              </Routes>
+          </ThemeProvider>
       </BrowserRouter>
   )
 }
